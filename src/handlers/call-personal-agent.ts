cda50a319b27dab4ff00d4f9876cf51cd94624a6
EresDev <eresdev@hotmail.com>
2024-11-29 12:45:11 +0500

    feat: init personal agent bridge

---
+++ src/handlers/call-personal-agent.ts
@@ -0,0 +1,63 @@
+import { Context } from "../types";
+
+/**
+ * NOTICE: Remove this file or use it as a template for your own plugins.
+ *
+ * This encapsulates the logic for a plugin if the only thing it does is say "Hello, world!".
+ *
+ * Try it out by running your local kernel worker and running the `yarn worker` command.
+ * Comment on an issue in a repository where your GitHub App is installed and see the magic happen!
+ *
+ * Logger examples are provided to show how to log different types of data.
+ */
+export async function callPersonalAgent(context: Context) {
+  const { logger, payload } = context;
+
+  const sender = payload.comment.user?.login;
+  const repo = payload.repository.name;
+  const issueNumber = payload.issue.number;
+  const owner = payload.repository.owner.login;
+  const body = payload.comment.body;
+
+  if (!body.match(/\B@([a-z0-9](?:-(?=[a-z0-9])|[a-z0-9]){0,38}(?<=[a-z0-9]))\s.*/gi)) {
+    logger.error(`Invalid use of personal agent command.`, { body });
+    return;
+  }
+
+  logger.info(`Comment received: ${body}`);
+
+  logger.debug(`Executing helloWorld:`, { sender, repo, issueNumber, owner });
+  // try {
+  //   await octokit.issues.createComment({
+  //     owner: payload.repository.owner.login,
+  //     repo: payload.repository.name,
+  //     issue_number: payload.issue.number,
+  //     body: configurableResponse,
+  //   });
+  //   if (customStringsUrl) {
+  //     const response = await fetch(customStringsUrl).then((value) => value.json());
+  //     await octokit.issues.createComment({
+  //       owner: payload.repository.owner.login,
+  //       repo: payload.repository.name,
+  //       issue_number: payload.issue.number,
+  //       body: response.greeting,
+  //     });
+  //   }
+  // } catch (error) {
+  //   /**
+  //    * logger.fatal should not be used in 9/10 cases. Use logger.error instead.
+  //    *
+  //    * Below are examples of passing error objects to the logger, only one is needed.
+  //    */
+  //   if (error instanceof Error) {
+  //     logger.error(`Error creating comment:`, { error: error, stack: error.stack });
+  //     throw error;
+  //   } else {
+  //     logger.error(`Error creating comment:`, { err: error, error: new Error() });
+  //     throw error;
+  //   }
+  // }
+
+  // logger.ok(`Successfully created comment!`);
+  logger.verbose(`Exiting callPersonalAgent`);
+}
--- src/plugin.ts
+++ src/plugin.ts
@@ -3,7 +3,7 @@ import { returnDataToKernel } from "./helpers/validator";
 import { Env, PluginInputs } from "./types";
 import { Context } from "./types";
 import { isIssueCommentEvent } from "./types/typeguards";
-import { helloWorld } from "./handlers/hello-world";
+import { callPersonalAgent } from "./handlers/call-personal-agent";
 import { LogLevel, Logs } from "@ubiquity-dao/ubiquibot-logger";
 
 /**
@@ -13,7 +13,7 @@ export async function runPlugin(context: Context) {
   const { logger, eventName } = context;
 
   if (isIssueCommentEvent(context)) {
-    return await helloWorld(context);
+    return await callPersonalAgent(context);
   }
 
   logger.error(`Unsupported event: ${eventName}`);
