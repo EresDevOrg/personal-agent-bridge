418e4ba239e17b9cffb8185dc0e4606fb005c6d6
EresDev <eresdev@hotmail.com>
2024-12-05 23:01:49 +0500

    fix: form workflow params on call
    logger is modifying params object. it prevents that

--- src/handlers/call-personal-agent.ts
+++ src/handlers/call-personal-agent.ts
@@ -11,7 +11,7 @@ import { Context } from "../types";
 export async function callPersonalAgent(context: Context) {
   const { logger, payload, octokit } = context;
 
-  //const owner = payload.repository.owner.login;
+  const owner = payload.repository.owner.login;
   const body = payload.comment.body;
 
   if (!body.match(/^\/\B@([a-z0-9](?:-(?=[a-z0-9])|[a-z0-9]){0,38}(?<=[a-z0-9]))\s.*/i)) {
@@ -27,19 +27,15 @@ export async function callPersonalAgent(context: Context) {
 
   const personalAgentOwner = targetUser[0].replace("/@", "");
 
-  logger.info(`Comment received: ${JSON.stringify({ username: personalAgentOwner, comment: body })}`);
-
-  const paWorkflowParams = {
-    owner: personalAgentOwner,
-    repo: "personal-agent",
-    workflow_id: "compute.yml",
-    ref: "development",
-  };
-
-  logger.debug(`Calling personal agent:`, paWorkflowParams);
+  logger.info(`Comment received:`, { commenter: owner, personalAgentOwner, comment: body });
 
   try {
-    await octokit.rest.actions.createWorkflowDispatch(paWorkflowParams);
+    await octokit.rest.actions.createWorkflowDispatch({
+      owner: personalAgentOwner,
+      repo: "personal-agent",
+      workflow_id: "compute.yml",
+      ref: "development",
+    });
   } catch (error) {
     logger.error(`Error dispatching workflow:`, { err: error, error: new Error() });
     throw error;
