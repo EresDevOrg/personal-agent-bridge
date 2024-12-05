ba14dd13ee46197783b13ef27868513332784d36
EresDev <eresdev@hotmail.com>
2024-12-05 22:15:29 +0500

    fix: reuse previous working func

--- src/handlers/call-personal-agent.ts
+++ src/handlers/call-personal-agent.ts
@@ -1,16 +1,21 @@
 import { Context } from "../types";
 
 /**
- * NOTICE: run the personal-agent repository workflow of mentioned user
+ * NOTICE: Remove this file or use it as a template for your own plugins.
  *
- * Given below is the accepted format of comment to command the personal agent of @exampleGithubUser
+ * This encapsulates the logic for a plugin if the only thing it does is say "Hello, world!".
  *
- * /@exampleGithubUser fork ubiquity-os/plugin-template
+ * Try it out by running your local kernel worker and running the `yarn worker` command.
+ * Comment on an issue in a repository where your GitHub App is installed and see the magic happen!
  *
+ * Logger examples are provided to show how to log different types of data.
  */
 export async function callPersonalAgent(context: Context) {
   const { logger, payload, octokit } = context;
 
+  const sender = payload.comment.user?.login;
+  const repo = payload.repository.name;
+  const issueNumber = payload.issue.number;
   const owner = payload.repository.owner.login;
   const body = payload.comment.body;
 
@@ -19,32 +24,64 @@ export async function callPersonalAgent(context: Context) {
     return;
   }
 
+  logger.info("logger info");
+  logger.error("logger error");
+  logger.debug("logger debug");
+
   const targetUser = body.match(/^\/\B@([a-z0-9](?:-(?=[a-z0-9])|[a-z0-9]){0,38}(?<=[a-z0-9]))/i);
   if (!targetUser) {
     logger.error(`Missing target username from comment: ${body}`);
     return;
   }
 
-  const personalAgentOwner = targetUser[0].replace("/@", "");
+  const username = targetUser[0].replace("/@", "");
 
-  logger.info(`Comment received: ${JSON.stringify({ username: owner, comment: body })}`);
+  logger.info(`Comment received: ${JSON.stringify({ username, comment: body })}`);
 
-  const paWorkflowParams = {
-    owner: personalAgentOwner,
-    repo: "personal-agent",
-    workflow_id: "compute.yml",
-    ref: "development",
-  };
-
-  logger.debug(`Calling personal agent:`, paWorkflowParams);
-
+  logger.debug(`Executing helloWorld:`, { sender, repo, issueNumber, owner });
   try {
-    await octokit.rest.actions.createWorkflowDispatch(paWorkflowParams);
+    await octokit.rest.actions.createWorkflowDispatch({
+      owner: "EresDevOrg",
+      repo: "personal-agent",
+      workflow_id: "compute.yml",
+      ref: "development",
+    });
   } catch (error) {
-    logger.error(`Error dispatching workflow:`, { err: error, error: new Error() });
-    throw error;
+    logger.info(`dispatch failed: ${error as string}`);
+    logger.error(`Error dispatching workflow: ${error as string}`);
   }
 
-  logger.ok(`Successfully sent the command to personal agent of @${personalAgentOwner}!`);
+  // try {
+  //   await octokit.issues.createComment({
+  //     owner: payload.repository.owner.login,
+  //     repo: payload.repository.name,
+  //     issue_number: payload.issue.number,
+  //     body: configurableResponse,
+  //   });
+  //   if (customStringsUrl) {
+  //     const response = await fetch(customStringsUrl).then((value) => value.json());
+  //     await octokit.issues.createComment({
+  //       owner: payload.repository.owner.login,
+  //       repo: payload.repository.name,
+  //       issue_number: payload.issue.number,
+  //       body: response.greeting,
+  //     });
+  //   }
+  // } catch (error) {
+  //   /**
+  //    * logger.fatal should not be used in 9/10 cases. Use logger.error instead.
+  //    *
+  //    * Below are examples of passing error objects to the logger, only one is needed.
+  //    */
+  //   if (error instanceof Error) {
+  //     logger.error(`Error creating comment:`, { error: error, stack: error.stack });
+  //     throw error;
+  //   } else {
+  //     logger.error(`Error creating comment:`, { err: error, error: new Error() });
+  //     throw error;
+  //   }
+  // }
+
+  // logger.ok(`Successfully created comment!`);
   logger.verbose(`Exiting callPersonalAgent`);
 }
