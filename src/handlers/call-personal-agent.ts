a69d67ab15ba811d46dba674dbf8eb7c5dad4264
EresDev <eresdev@hotmail.com>
2024-12-05 22:28:39 +0500

    fix: log correct user name

--- src/handlers/call-personal-agent.ts
+++ src/handlers/call-personal-agent.ts
@@ -11,7 +11,7 @@ import { Context } from "../types";
 export async function callPersonalAgent(context: Context) {
   const { logger, payload, octokit } = context;
 
-  const owner = payload.repository.owner.login;
+  //const owner = payload.repository.owner.login;
   const body = payload.comment.body;
 
   if (!body.match(/^\/\B@([a-z0-9](?:-(?=[a-z0-9])|[a-z0-9]){0,38}(?<=[a-z0-9]))\s.*/i)) {
@@ -27,7 +27,7 @@ export async function callPersonalAgent(context: Context) {
 
   const personalAgentOwner = targetUser[0].replace("/@", "");
 
-  logger.info(`Comment received: ${JSON.stringify({ username: owner, comment: body })}`);
+  logger.info(`Comment received: ${JSON.stringify({ username: personalAgentOwner, comment: body })}`);
 
   const paWorkflowParams = {
     owner: personalAgentOwner,
