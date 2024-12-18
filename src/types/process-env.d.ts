620a54ab0728ddd37b5aba6b040f9bd6b33b89f8
EresDev <eresdev@hotmail.com>
2024-12-18 22:00:45 +0500

    fix: use existing X25519_PRIVATE_KEY

--- src/types/process-env.d.ts
+++ src/types/process-env.d.ts
@@ -2,7 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       GITHUB_TOKEN: string;
-      PA_BRIDGE_X25519_PRIVATE_KEY: string;
+      X25519_PRIVATE_KEY: string;
     }
   }
 }
