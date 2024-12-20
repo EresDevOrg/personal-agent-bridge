21deea2817ad8870ccc29f532bcdd99793ecd085
EresDev <eresdev@hotmail.com>
2024-12-20 17:13:32 +0500

    refactor: rename vars, remove prefix underscore

--- src/helpers/keys.ts
+++ src/helpers/keys.ts
@@ -9,11 +9,10 @@ export async function decryptKeys(
 ): Promise<{ decryptedText: string; publicKey: string } | { decryptedText: null; publicKey: null }> {
   await sodium.ready;
 
-  let _public: null | string = null;
-  const _private: null | string = null;
+  let publicKey: null | string = null;
 
-  _public = await getScalarKey(x25519PrivateKey);
-  if (!_public) {
+  publicKey = await getScalarKey(x25519PrivateKey);
+  if (!publicKey) {
     logger.error("Public key is null");
     return { decryptedText: null, publicKey: null };
   }
@@ -21,14 +20,14 @@ export async function decryptKeys(
     logger.error("No cipherText was provided");
     return { decryptedText: null, publicKey: null };
   }
-  const binaryPublic = sodium.from_base64(_public, sodium.base64_variants.URLSAFE_NO_PADDING);
+  const binaryPublic = sodium.from_base64(publicKey, sodium.base64_variants.URLSAFE_NO_PADDING);
   const binaryPrivate = sodium.from_base64(x25519PrivateKey, sodium.base64_variants.URLSAFE_NO_PADDING);
 
   const binaryCipher = sodium.from_base64(cipherText, sodium.base64_variants.URLSAFE_NO_PADDING);
 
   const decryptedText: string | null = sodium.crypto_box_seal_open(binaryCipher, binaryPublic, binaryPrivate, "text");
 
-  return { decryptedText: decryptedText, publicKey: _public };
+  return { decryptedText: decryptedText, publicKey: publicKey };
 }
 
 async function getScalarKey(x25519PrivateKey: string) {
