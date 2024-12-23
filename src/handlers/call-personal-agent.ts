916ea183dd95efe20154deb655995607c75d4f73
EresDev <eresdev@hotmail.com>
2024-12-23 17:26:03 +0500

    fix(fixup): keep original property name of event payload

--- src/handlers/call-personal-agent.ts
+++ src/handlers/call-personal-agent.ts
@@ -56,7 +56,7 @@ export async function callPersonalAgent(context: Context, inputs: PluginInputs) 
       repo: "personal-agent",
       workflow_id: "compute.yml",
       ref: "development",
-      inputs: { ...inputs, eventPayload: {}, settings: {}, initialEventPayload: JSON.stringify(inputs.eventPayload) } as unknown as { [key: string]: unknown },
+      inputs: { ...inputs, settings: {}, eventPayload: JSON.stringify(inputs.eventPayload) } as unknown as { [key: string]: unknown },
     });
   } catch (error) {
     logger.error(`Error dispatching workflow: ${error}`);
